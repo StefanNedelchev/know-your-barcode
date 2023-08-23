02954537240df42a8f335c47a8921a6d21f22e7d
Stefan <hardmaster.92@hotmail.com>
2023-08-23 11:54:18 +0300

    update production url for netlify functions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  functionsBaseUrl: 'https://friendly-trifle-4f46b0.netlify.app/.netlify/functions',
+  functionsBaseUrl: 'https://know-your-barcode.netlify.app/.netlify/functions',
 };
