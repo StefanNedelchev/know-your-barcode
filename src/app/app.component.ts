d0c630bea32425d1e16c83a8f242ea72a1f5c0b2
Stefan <hardmaster.92@hotmail.com>
2023-08-23 12:14:38 +0300

    added app update dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 import { BarcodeScannerComponent } from './components/barcode-scanner/barcode-scanner.component';
 import { BarcodeResultsComponent } from './components/barcode-results/barcode-results.component';
 import { BarcodeScannerResult } from './core/models';
@@ -11,6 +13,22 @@ import { BarcodeScannerResult } from './core/models';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public scannerResult: BarcodeScannerResult = { barcodes: [], instantSearch: false };
+
+  constructor(private readonly swUpdate: SwUpdate) {}
+
+  public ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.pipe(
+        filter((versionUpdate) => versionUpdate.type === 'VERSION_READY'),
+      ).subscribe(() => {
+        // eslint-disable-next-line no-alert
+        if (window.confirm('A new version of Know Your Barcode has been insetalled and ready to use! '
+          + 'Do you want to refresh the app and let the new changes take effect?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
